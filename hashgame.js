5a6d61d811f5241dffa65ffb2b00cde303bf129b
jaomisterio <joaogamerpolado@gmail.com>
2022-05-04 06:07:08 -0300

    fix bugs

--- hashgame.js
+++ hashgame.js
@@ -1,8 +1,9 @@
+//need readline-sync since im not going to write 1000 lines of code that I don't even understand
 const input = require('readline-sync')
 
-
-const wannaPlay = input.question('ola gostaria de jogar?\n\n(sim/nao)\n\nR: ')
-
+const wannaPlay = () => {let banana = input.question('ola gostaria de jogar?\n\n(sim/nao)\n\nR: '); 
+                if(banana === 'sim'){startGame()}}
+                
 String.prototype.replaceAt = function(index, replacement) {
     return this.substring(0, index) + replacement + this.substring(index + replacement.length);
 }
@@ -14,54 +15,40 @@ const startGame = async () => {
     console.log(gameDraw)
     for(let i = gamePossibilities.length; i!== 0; i--){
         function checkEnd(){
-            //cross
-            if (gameDraw[0] == ('X' || 'O') && gameDraw[34] == ('X' || 'O') && gameDraw[68] == ('X' || 'O')){
-                console.log('game ended')
+            if ((gameDraw[0] == ('X') && gameDraw[34] == ('X') && gameDraw[68] == ('X')) || 
+            (gameDraw[12] == ('X') && gameDraw[34] == ('X') && gameDraw[56] == ('X')) ||
+            (gameDraw[0] == ('X') && gameDraw[6] == ('X') && gameDraw[12] == ('X')) ||
+            (gameDraw[28] == ('X') && gameDraw[34] == ('X') && gameDraw[40] == ('X')) ||
+            (gameDraw[56] == ('X') && gameDraw[62] == ('X') && gameDraw[68] == ('X')) ||
+            (gameDraw[0] == ('X') && gameDraw[28] == ('X') && gameDraw[56] == ('X')) ||
+            (gameDraw[6] == ('X') && gameDraw[34] == ('X') && gameDraw[62] == ('X')) ||
+            (gameDraw[12] == ('X') && gameDraw[40] == ('X') && gameDraw[68] == ('X')))
+            {
                 return true
                 }
-            if (gameDraw[12] == ('X' || 'O') && gameDraw[34] == ('X' || 'O') && gameDraw[56] == ('X' || 'O')){
-                console.log('game ended')
-                return true
-                }
-            //horizontal
-            if (gameDraw[0] == ('X' || 'O') && gameDraw[6] == ('X' || 'O') && gameDraw[12] == ('X' || 'O')){
-                console.log('game ended')
-                return true
-                }
-            if (gameDraw[28] == ('X' || 'O') && gameDraw[34] == ('X' || 'O') && gameDraw[40] == ('X' || 'O')){
-                console.log('game ended')
-                return true
-                }
-            if (gameDraw[56] == ('X' || 'O') && gameDraw[62] == ('X' || 'O') && gameDraw[68] == ('X' || 'O')){
-                console.log('game ended')
-                return true
-                }
-            //vertical
-            if (gameDraw[0] == ('X' || 'O') && gameDraw[28] == ('X' || 'O') && gameDraw[56] == ('X' || 'O')){
-                console.log('game ended')
-                return true
-                }
-            if (gameDraw[6] == ('X' || 'O') && gameDraw[34] == ('X' || 'O') && gameDraw[62] == ('X' || 'O')){
-                console.log('game ended')
-                return true
-                }
-            if (gameDraw[12] == ('X' || 'O') && gameDraw[40] == ('X' || 'O') && gameDraw[68] == ('X' || 'O')){
-                console.log('game ended')
+             if ((gameDraw[0] == ('O') && gameDraw[34] == ('O') && gameDraw[68] == ('O')) || 
+            (gameDraw[12] == ('O') && gameDraw[34] == ('O') && gameDraw[56] == ('O')) ||
+            (gameDraw[0] == ('O') && gameDraw[6] == ('O') && gameDraw[12] == ('O')) ||
+            (gameDraw[28] == ('O') && gameDraw[34] == ('O') && gameDraw[40] == ('O')) ||
+            (gameDraw[56] == ('O') && gameDraw[62] == ('O') && gameDraw[68] == ('O')) ||
+            (gameDraw[0] == ('O') && gameDraw[28] == ('O') && gameDraw[56] == ('O')) ||
+            (gameDraw[6] == ('O') && gameDraw[34] == ('O') && gameDraw[62] == ('O')) ||
+            (gameDraw[12] == ('O') && gameDraw[40] == ('O') && gameDraw[68] == ('O')))
+            {
                 return true
                 }
             }
-            if (checkEnd() == true) return;
     if (coin == 1){
     coin = coin + 1
-    console.log(coin)
-    const userATurn = await input.keyInSelect(gamePossibilities, 'Jogador 2 onde deseja posicionar?')
+    const userATurn = await input.keyInSelect(gamePossibilities, 'Jogador 1 onde deseja posicionar?')
     indexToReplace = gameDraw.indexOf(gamePossibilities[userATurn])
     gamePossibilities.splice(userATurn, 1)
     gameDraw.replaceAt(indexToReplace, 'X')
     gameDraw = gameDraw.split('');
     gameDraw[indexToReplace] = 'X';
     gameDraw = gameDraw.join('');
-    console.log(gameDraw)
+    console.log(`${gameDraw}\n\n`)
+    if (checkEnd() == true) return console.log('Jogador 1 venceu!, FIM DE JOGO!'), wannaPlay();
     continue
     }
     if (coin == 2){
@@ -73,16 +60,10 @@ const startGame = async () => {
     gameDraw = gameDraw.split('');
     gameDraw[indexToReplace] = 'O';
     gameDraw = gameDraw.join('');
-    console.log(gameDraw)
+    console.log(`${gameDraw}\n\n`)
+    if (checkEnd() == true) return console.log('Jogador 2 venceu!, FIM DE JOGO!'), wannaPlay();
     continue
         }
     }
 }
-const gamechooseer = () => {
-    const options = ['jogo da velha' , 'quero me matar', 'vou me matar hoje de noite']   
-    const selected = input.keyInSelect(options, 'que jogo?')
-    console.log(`you choosed ${options[selected]}`)
-    if (selected === 0){startGame()}
-    }
-
-if (wannaPlay === 'sim'){gamechooseer()}
+wannaPlay()
